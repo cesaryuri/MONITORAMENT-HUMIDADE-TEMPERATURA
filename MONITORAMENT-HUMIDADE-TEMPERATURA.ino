0e66aeadd7128af1bd273f0641e962fbdeb59077
Cesar Yuri e Silva Santos <91036277+cesaryuri@users.noreply.github.com>
2024-05-25 19:55:40 -0300

    ADD_DTH-WITH-DISPLAY

--- MONITORAMENT-HUMIDADE-TEMPERATURA.ino
+++ MONITORAMENT-HUMIDADE-TEMPERATURA.ino
@@ -1,86 +1,55 @@
-
-
-//Carrega a biblioteca LiquidCrystal
 #include <LiquidCrystal.h>
- 
-//Define os pinos que serão utilizados para ligação ao display
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 #include "DHT.h"
-#define DHTPIN 1    
-#define DHTTYPE DHT11   
-float t = 0;
-float h = 0;
 
+// Define os pinos para o LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Configuração do sensor DHT
+#define DHTPIN 13    
+#define DHTTYPE DHT11   
 DHT dht(DHTPIN, DHTTYPE);
+
+float t = 0; // Temperatura
+float h = 0; // Umidade
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
-  //Define o número de colunas e linhas do LCD
-  lcd.begin(16, 2);
-  dht.begin();
+  
+  lcd.begin(16, 2); // Inicializa o display LCD
+  dht.begin(); // Inicializa o sensor DHT
 }
-
 
 void loop() {
-    // Wait a few seconds between measurements.
-    delay(2000);
-    
-    tela();
-    //rundth();
-
+  delay(2000);
+  runDHT();
+  displayData();
 }
 
+void runDHT(){
+  h = dht.readHumidity(); 
+  t = dht.readTemperature(); 
 
-
-
-//rodar DTH e printar
-void rundth(){
-    
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  t = dht.readTemperature();
-  
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
+  if (isnan(h) || isnan(t)) { 
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
+  
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
-  Serial.print(t);
+  Serial.println(t);
 }
 
+void displayData() {
+  lcd.clear(); 
+  lcd.setCursor(0, 0); 
+  lcd.print("TEMP: ");
+  lcd.print(t);
+  lcd.print(" C");
 
-void tela()
-{
-  //Limpa a tela
-  lcd.clear();
-  //Posiciona o cursor na coluna 3, linha 0;
-  lcd.setCursor(3, 0);
-  //Envia o texto entre aspas para o LCD
-  lcd.print("COMPILANDO......");
-  lcd.setCursor(3, 1);
-  lcd.print("RODOUUUU");
-  delay(5000);
-    
-  //Rolagem para a esquerda
-  for (int posicao = 0; posicao < 3; posicao++)
-  {
-    lcd.scrollDisplayLeft();
-    delay(300);
-  }
-    
-  //Rolagem para a direita
-  for (int posicao = 0; posicao < 6; posicao++)
-  {
-    lcd.scrollDisplayRight();
-    delay(300);
-  }
+  lcd.setCursor(0, 1);
+  lcd.print("HUMI: ");
+  lcd.print(h);
+  lcd.print(" %");
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,119 +1,88 @@
 # 1 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 2 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 2
+# 3 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 2
 
-
-//Carrega a biblioteca LiquidCrystal
-# 5 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 2
-
-//Define os pinos que serão utilizados para ligação ao display
+// Define os pinos para o LCD
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-# 9 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 2
+
+// Configuração do sensor DHT
 
 
-float t = 0;
-float h = 0;
+DHT dht(13, 11 /**< DHT TYPE 11 */);
 
-DHT dht(1, 11 /**< DHT TYPE 11 */);
+float t = 0; // Temperatura
+float h = 0; // Umidade
 
 void setup() {
   Serial.begin(9600);
   Serial.println((reinterpret_cast<const __FlashStringHelper *>(
-# 18 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 17 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
                 (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 18 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 17 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
                 "DHTxx test!"
-# 18 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 17 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
                 ); &__c[0];}))
-# 18 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 17 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
                 )));
-  //Define o número de colunas e linhas do LCD
-  lcd.begin(16, 2);
-  dht.begin();
-}
 
+  lcd.begin(16, 2); // Inicializa o display LCD
+  dht.begin(); // Inicializa o sensor DHT
+}
 
 void loop() {
-    // Wait a few seconds between measurements.
-    delay(2000);
-
-    tela();
-    //rundth();
-
+  delay(2000);
+  runDHT();
+  displayData();
 }
 
-
-
-
-//rodar DTH e printar
-void rundth(){
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+void runDHT(){
   h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   t = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println((reinterpret_cast<const __FlashStringHelper *>(
-# 48 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 34 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
                   (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 48 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 34 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
                   "Failed to read from DHT sensor!"
-# 48 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 34 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
                   ); &__c[0];}))
-# 48 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 34 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
                   )));
     return;
   }
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
 
   Serial.print((reinterpret_cast<const __FlashStringHelper *>(
-# 54 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 38 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
               (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 54 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 38 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
               "Humidity: "
-# 54 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 38 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
               ); &__c[0];}))
-# 54 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 38 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
               )));
   Serial.print(h);
   Serial.print((reinterpret_cast<const __FlashStringHelper *>(
-# 56 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 40 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
               (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 56 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 40 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
               "%  Temperature: "
-# 56 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
+# 40 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino" 3
               ); &__c[0];}))
-# 56 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
+# 40 "/home/cesar/Documentos/GitHub/MONITORAMENT-HUMIDADE-TEMPERATURA/MONITORAMENT-HUMIDADE-TEMPERATURA.ino"
               )));
-  Serial.print(t);
+  Serial.println(t);
 }
 
-
-void tela()
-{
-  //Limpa a tela
+void displayData() {
   lcd.clear();
-  //Posiciona o cursor na coluna 3, linha 0;
-  lcd.setCursor(3, 0);
-  //Envia o texto entre aspas para o LCD
-  lcd.print("COMPILANDO......");
-  lcd.setCursor(3, 1);
-  lcd.print("RODOUUUU");
-  delay(5000);
+  lcd.setCursor(0, 0);
+  lcd.print("TEMP: ");
+  lcd.print(t);
+  lcd.print(" C");
 
-  //Rolagem para a esquerda
-  for (int posicao = 0; posicao < 3; posicao++)
-  {
-    lcd.scrollDisplayLeft();
-    delay(300);
-  }
-
-  //Rolagem para a direita
-  for (int posicao = 0; posicao < 6; posicao++)
-  {
-    lcd.scrollDisplayRight();
-    delay(300);
-  }
+  lcd.setCursor(0, 1);
+  lcd.print("HUMI: ");
+  lcd.print(h);
+  lcd.print(" %");
 }
